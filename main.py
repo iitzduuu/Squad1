f10163a716110cc3cc06c58004f58eab1454b2b0
Paula Suyane <paulasuyaneprojeto2023@gmail.com>
2025-06-21 20:03:27 -0300

    code test

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ def main():
     print(user_valid)
 if __name__ == "__main__":
     main()
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    nome = data.get('nome')
+    senha = data.get('senha')
+
+    if nome in users and users[nome] == senha:
+        return jsonify({'mensagem': 'Login válido!'})
+    else:
+        return jsonify({'mensagem': 'Usuário inválido'})
